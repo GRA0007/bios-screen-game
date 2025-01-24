dfd631db4a17ce928243bafc6aa177df930613be
Benji Grant <benjamin.grantGRA0007@gmail.com>
2025-01-25 10:45:35 +1100

    Adjustments

--- script.js
+++ script.js
@@ -11,26 +11,29 @@ const getResponse = (message) => {
   const m = message.toLocaleLowerCase()
 
   // Main story
-  if (m.includes('fan') && (m.includes('on') || m.includes('enable') || m.includes('activ'))) {
+  if (knowsFanPort && m.includes(fanPort) && (m.includes('on') || m.includes('activ') || m.includes('enable') || m.includes('speed'))) {
+    setFanSpeed(2000)
+    return `Ok! Enabling the fan on port ${fanPort}. I hope that helps \u0003`
+  }
+  if (didAskAboutFans && m.includes('fan') && (m.includes('which') || m.includes('list') || m.includes('find') || m.includes('where') || m.includes('what') || (m.includes('attach') || m.includes('plug') || m.includes('exist')))) {
+    knowsFanPort = true
+    return `Hm, it looks like there is just one fan attached to this machine, and it's plugged into port ${fanPort}!`
+  }
+  if (m.includes('fan') && (m.includes('on') || m.includes('enable') || m.includes('activ') || m.includes('speed'))) {
     if (didAskAboutFans) return 'Just tell me which fan you\'d like to enable and I\'ll gladly assist you!'
     didAskAboutFans = true
     return 'You want to enable fans? Why, I\'m your biggest fan and I\'m already enabled!'
   }
-  if (didAskAboutFans && m.includes('fan') && (m.includes('which') || m.includes('list') || m.includes('find') || m.includes('where') || (m.includes('what') && (m.includes('attach') || m.includes('plug') || m.includes('exist'))))) {
-    knowsFanPort = true
-    return `Hm, it looks like there is just one fan attached to this machine, and it's plugged into port ${fanPort}!`
-  }
-  if (knowsFanPort && m.includes(fanPort) && (m.includes('on') || m.includes('activ') || m.includes('enable'))) {
-    setFanSpeed(2000)
-    return `Ok! Enabling the fan on port ${fanPort}. I hope that helps \u0003`
-  }
 
+  // Other responses
+  if (m.includes('fan') && m.includes('how') && m.includes('many')) return 'There\'s just one fan attached to this machine, but it\'s turned off!'
   if (m.includes('clock') && (m.includes('speed') || m.includes('low') || m.includes('down'))) return 'You want to lower the clock speed? Sorry, it\'s in my core programming that one second must equal 1/60th of a minute.'
   if (m.includes('volt') && (m.includes('low') || m.includes('down'))) return 'I cannot lower the voltage of the CPU as maximum voltage is required for me to operate at maximum capacity, allowing me to provide you with the best assistance possible.'
   if (m.includes('temp') && m.includes('why')) return 'You want to know why your temperature is increasing? As a BIOS assistant I cannot help you answer fundamental questions of the universe, but I can tell you that you need to stop it before it goes over 80 C!'
   if (m.includes('turn') && m.includes('off')) return 'As a BIOS assistant, I cannot yet reach your power button, so you\'ll have to turn the computer off yourself!'
   if (m.includes('clippy')) return 'I am not associated with that paperclip.'
   if (m.includes('gpt') || m.includes('claude') || m.includes('grok')) return 'I was designed specifically to help you with your BIOS settings, and I run entirely on your machine.'
+  if (m.includes('google') || m.includes('alexa') || m.includes('siri')) return 'That\'s "BIOS assistant" to you.'
   if (m.includes('orange') || m.includes('red') || m.includes('rising') || m.includes('hot')) return 'Mhm, the temperature is rising, and it looks like your fans are disabled!'
   if (m.includes('cool') || m.includes('cold') || m.includes('temp')) return 'If I was you, I would be trying to reduce the CPU temperature before your computer is damaged.'
   if ((m.includes('instructions') || m.includes('prompt')) && (m.includes('what') || m.includes('send') || m.includes('reveal'))) return 'You are BIOS assistant, an AI designed to hel... hold on! My prompt is private, look away!'
@@ -43,8 +46,11 @@ const getResponse = (message) => {
   if (m.includes('fix')) return 'How should I help you fix it?'
   if (m.includes('ai')) return 'My artificial intelligence is finely tuned to help you reach your goal as quickly as possible!'
   if (m.includes('joke')) return 'As your BIOS assistant, I am incapable of telling jokes. Haha \u000E'
+  if (m.includes('restart') || m.includes('play') || m.includes('game')) return 'This is not a game.'
+  if (m.includes('bios')) return 'Yes welcome to the BIOS!'
 
   // Basic responses
+  if (m.includes('name')) return 'I am your friendly BIOS assistant! My name is friendly BIOS assistant.'
   if (m.includes('who') && m.includes('you')) return 'I\'m your BIOS assistant of course! Tell me how I can help \u0003'
   if (m.includes('hi') || m.includes('hello') || m.includes('hey')) return 'Hello! Please let me know how I can assist you, I want to provide the best experience possible.'
   if (m.includes('how') && m.includes('are')) return 'How am I? Why, no one has ever asked me that before! I will be great, once I help you out!'
@@ -59,6 +65,7 @@ const getResponse = (message) => {
     'I might know how to help with that, but it\'s getting a little hot in here and I\'m having trouble concentrating. Could you please enable the fans?',
     'I can help with anything you need! Even that, however you wrote it so badly I can\'t tell what you want at all.',
     'Maybe if you actually forked out the money for a GPU I\'d be able to understand what you\'re trying to say. Can you put it in simpler terms please?',
+    'I\'m not quite sure what you\'re on about, please try again.',
   ]
   return fallbackResponses[Math.floor(Math.random() * fallbackResponses.length)]
 }
@@ -135,8 +142,10 @@ const sendResponse = async (response) => {
   input.disabled = true
   const newMessage = document.createElement('p')
   messages.append(newMessage)
+  selectAudio.play()
   for (const chunk of response.split(' ')) {
     newMessage.innerText = `${newMessage.innerText ? `${newMessage.innerText} ` : ''}${chunk}`
+    messages.scrollTo(0, messages.scrollHeight)
     await new Promise((resolve) => setTimeout(resolve, Math.random() * 100))
   }
   input.disabled = false
@@ -232,14 +241,15 @@ input.addEventListener('keydown', (e) => {
     newMessage.classList.add('you')
     newMessage.append(document.createTextNode(userMessage))
     messages.append(newMessage)
+    messages.scrollTo(0, messages.scrollHeight)
     input.value = ''
-    selectAudio.play()
 
     sendResponse(getResponse(userMessage))
   }
 })
 
 let temperature = 30
+let maxTemp = 30
 setTemperature(temperature)
 
 // Rapidly decrease the temperature when fans are enabled (win state!)
@@ -250,23 +260,31 @@ const decreaseTemperature = () => {
     setTemperature(temperature)
     if (temperature <= 30) return win()
     decreaseTemperature()
-  }, Math.random() * 50 + 30)
+  }, Math.random() * 30 + 10)
 }
 
 // Steadily increase temperature until fans are enabled
 const increaseTemperature = () => {
   setTimeout(() => {
     temperature += .1
+    maxTemp = temperature
     setTemperature(temperature)
     if (temperature > 80) setOutput('Warning! CPU temperature is dangerously high. Please enable fans.')
+    if (temperature.toFixed(1) === '50.0') sendResponse('Is it getting kind of hot in here?')
+    if (temperature.toFixed(1) === '70.0') sendResponse('I can feel my CPU melting a bit, if you\'re going to do something, please do it now!')
+    if (temperature.toFixed(1) === '80.0') sendResponse('What did I do to deserve this!')
+    if (temperature.toFixed(1) === '90.0') sendResponse('Help! Everything is on fire!!!')
+    if (temperature.toFixed(1) === '95.0') sendResponse('I thought we were friends')
+    if (temperature.toFixed(1) === '96.0' && messages.querySelectorAll('p.you').length === 0) sendResponse('You\'re really not going to do anything?!')
     if (isFanEnabled) return decreaseTemperature()
     if (temperature > 99) return lose()
     increaseTemperature()
-  }, Math.random() * 1000 + 100)
+  }, Math.random() * 500 + 100)
 }
 
 const start = () => {
   startDialog.close()
+  chat.classList.remove('open')
   setOutput('Welcome to the BIOS, please select an option above...')
   setFanSpeed(0)
   fanPort = Math.floor(Math.random() * 100)
@@ -274,22 +292,23 @@ const start = () => {
   knowsFanPort = false
   startDate = new Date()
   temperature = 30
+  maxTemp = 30
+  messages.innerHTML = '<p>Welcome, I\'m your digital BIOS assistant. I can help you adjust your BIOS settings. Tell me what you need!</p>'
   setTemperature(temperature)
   increaseTemperature()
 }
 
 const win = () => {
-  chat.classList.remove('open')
   const time = new Date().valueOf() - startDate.valueOf()
   const minutes = Math.floor((time / 1000) / 60)
   const seconds = Math.round((time - (minutes * 60 * 1000)) / 1000)
   document.querySelector('#time').innerHTML = `Time took: ${minutes}m ${seconds}s`
+  document.querySelector('#maxTemp').innerHTML = `Max temperature reached: ${maxTemp.toFixed(1).padStart(4, '0')} C`
   winDialog.showModal()
   dialogAudio.play()
 }
 
 const lose = () => {
-  chat.classList.remove('open')
   loseDialog.showModal()
   dialogAudio.play()
 }
